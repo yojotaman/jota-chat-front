6c626686d1f83f7a470f8b6605cb9ebcddf98d6c
yojotaman <yojotaman@gmail.com>
2017-03-25 20:53:40 -0500

    Se añaden las picture cards, se agrega funcionalidad para los likes y se añade formato a las fechas y mensajes usando format js

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,30 @@ var title = require('title');
 page('/', function(ctx, next){
     title('Portal SF')
     var main = document.getElementById('main-container');
-	empty(main).appendChild(template);
+
+    var pictures = [ 
+        {
+            user: {
+                username: 'mariac',
+                avatar: 'http://www.stockvault.net/data/2014/06/19/158950/preview16.jpg'
+            },
+            url:'chaqueta.jpg',
+            likes: 0,
+            liked:false,
+            createdAt: new Date()
+        },
+        {
+            user: {
+                username: 'mariac',
+                avatar: 'http://www.stockvault.net/data/2014/06/19/158950/preview16.jpg'
+            },
+            url:'chaqueta.jpg',
+            likes: 1,
+            liked:true,
+            createdAt: new Date().setDate(new Date().getDate()-10)
+        },
+        
+        ];
+
+	empty(main).appendChild(template(pictures));
 })
