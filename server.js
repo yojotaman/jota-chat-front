63107ddd3b9455f1a99796bec079d26c6f43635d
yojotaman <yojotaman@gmail.com>
2017-04-19 13:37:20 -0500

    se realiza la configuración de middleware en express y se instalan las dependencias para la funcionalidad de autenticación

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 var express = require('express');
-var multer=require('multer');
-var ext = require('file-extension');
-
-
-var config = require('./config');
 var aws = require('aws-sdk');
+var multer=require('multer');
 var multerS3 = require('multer-s3');
+var ext = require('file-extension');
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser');
+var expressSession = require('express-session')
+var passport = require('passport');
+var config = require('./config');
+var port = process.env.PORT || 3000;
+
 
 var s3 = new aws.S3({
   accessKeyId: config.aws.accessKey,
   secretAccessKey: config.aws.secretKey
 });
-
-// almacenamiento antiguo en disco local
-// var storage = multer.diskStorage({
-//     destination: function(req, file, cb){
-//         cb(null, './uploads')
-//     },
-//     filename: function (req, file, cb){
-//         cb(null, Date.now()+'.'+ext(file.originalname));
-//     }
-// })
-
 
 // nuevo almacenamiento en bucket S3 de AWS
 var storage = multerS3({
@@ -36,10 +29,21 @@ var storage = multerS3({
   }
 });
 
-
-
 var upload = multer({storage: storage}).single('picture');
+
 var app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(expressSession({
+  secret: config.secret,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.set('view engine', 'pug');
 
@@ -150,7 +154,10 @@ app.get('/:username/:id', function(req, res){
     res.render('index', { title: `StudioF - ${req.params.username}`})
 })
 
-app.listen (3000, function (err){
-	if (err) return console.log('Hubo un error'), process.exit(1);
-	console.log ('Portal_StudioF escuchando en el puerto 3000');
+app.listen (port, function (err){
+	if (err) {
+        return console.log('Hubo un error')
+        process.exit(1);
+    }
+	console.log (`Portal_StudioF escuchando en el puerto ${port}`);
 })
